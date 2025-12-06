41bf1765a9176c0a490b98e768c26952919af301
Hemanth k <118145147+Hemanth-slayer@users.noreply.github.com>
2025-12-07 04:04:39 +0530

    Update log_cleanup.sh

--- log_cleanup.sh
+++ log_cleanup.sh
@@ -1,1 +1,9 @@
 
+#!/bin/bash
+
+LOG_DIR="/var/log"
+DAYS=5
+
+echo "Cleaning logs older than $DAYS days from $LOG_DIR..."
+find "$LOG_DIR" -type f -mtime +$DAYS -exec rm -f {} \;
+echo "Log cleanup completed!"
